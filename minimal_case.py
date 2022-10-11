f099daebc26af98ceff943c9e90654dd85667995
Thomas Gordon <nrgetik@users.noreply.github.com>
2022-10-11 10:57:50 -0400

    ASGI structure
    Signed-off-by: Thomas Gordon <nrgetik@users.noreply.github.com>

--- minimal_case.py
+++ minimal_case.py
@@ -1,10 +1,16 @@
-# from json import dumps
-from pprint import pprint
+async def app(scope, receive, send):
+    if scope["type"] != "http":
+        raise Exception("Only the HTTP protocol is supported")
 
-def main(*args):
-    print(type(args))
-    print(len(args))
-    pprint(args)
-    # kwargs["positional"] = args
-    # return dumps(kwargs)
-    # return { "statusCode": 200, "body": "Status OK" }
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            (b'content-type', b'text/plain'),
+            (b'content-length', b'5'),
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'hello',
+    })
