cc6873534b286ca4eacfd5ed02270a8d4c175f7e
Thomas Gordon <nrgetik@users.noreply.github.com>
2022-10-11 15:18:08 -0400

    Build out handler
    Signed-off-by: Thomas Gordon <nrgetik@users.noreply.github.com>

--- starter_ops.py
+++ starter_ops.py
@@ -1,7 +1,9 @@
+from base64 import b64encode
+from json import dumps
 from os import environ
 from flask import Flask
 from flask import request
-from requests import get, post, put
+import requests
 
 GH_ORG_NAME = environ["GH_ORG_NAME"]
 GH_USER_NAME = environ["GH_USER_NAME"]
@@ -34,18 +36,21 @@ def gh_request(method, resource, payload=None, addtl_headers=None):
     full_url = f"{gh_rest_base_url}{resource}"
     gh_base_headers = {"Accept": "application/vnd.github.v3+json"}
     full_headers = gh_base_headers|addtl_headers if addtl_headers else gh_base_headers
-    match method:
-        case "GET":
-            res = get(full_url, headers=full_headers,
-                    auth=(GH_USER_NAME, GH_ACCESS_TOKEN))
-        case "POST":
-            res = post(full_url, headers=full_headers, data=payload,
-                    auth=(GH_USER_NAME, GH_ACCESS_TOKEN))
-        case "PUT":
-            res = put(full_url, headers=full_headers, data=payload,
-                    auth=(GH_USER_NAME, GH_ACCESS_TOKEN))
-        case _:
-            return False
+    try:
+        match method:
+            case "GET":
+                res = requests.get(full_url, headers=full_headers,
+                        auth=(GH_USER_NAME, GH_ACCESS_TOKEN))
+            case "POST":
+                res = requests.post(full_url, headers=full_headers, data=payload,
+                        auth=(GH_USER_NAME, GH_ACCESS_TOKEN))
+            case "PUT":
+                res = requests.put(full_url, headers=full_headers, data=payload,
+                        auth=(GH_USER_NAME, GH_ACCESS_TOKEN))
+        res.raise_for_status()
+    except requests.exceptions.RequestException:
+        # do some more sophisticated error handling
+        pass
     return res
 
 app = Flask(__name__)
@@ -70,7 +75,29 @@ def oven():
     and takes the necessary actions to issue our protections using the
     GitHub REST API
     """
-    return request.json
+    event_dict = request.json
+
+    # GH will push to us on a few repo events, but we only care when a repo is created
+    if event_dict["action"] == "created":
+        owner = event_dict["repository"]["owner"]["login"]
+        repo = event_dict["repository"]["name"]
+
+        branches = gh_request("GET", f"/repos/{owner}/{repo}/branches").json()
+        if len(branches) > 0:
+            # if we already have a default/main branch, protect it
+            branch = branches[0]["name"]
+            gh_request("PUT", f"/repos/{owner}/{repo}/branches/{branch}/protection",
+                    payload=dumps(PROTECTIONS_PAYLOAD_DICT))
+        else:
+            # otherwise, commit a README.md to create a default branch, then protect it
+            contents = b64encode(f"# {repo}".encode("utf-8")).decode("utf-8").strip("\n")
+            commit_dict = {"branch": "main", "message": "Initial commit", "content": contents}
+            gh_request("PUT", f"/repos/{owner}/{repo}/contents/README.md",
+                payload=dumps(commit_dict))
+            gh_request("PUT", f"/repos/{owner}/{repo}/branches/main/protection",
+                payload=dumps(PROTECTIONS_PAYLOAD_DICT))
+        # log an issue to the repo describing actions taken
+        gh_request("POST", f"/repos/{owner}/{repo}/issues", payload=dumps(ISSUE_PAYLOAD_DICT))
 
 if __name__ == "__main__":
     app.run(use_reloader=True)
