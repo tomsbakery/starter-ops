103fb8cc26579d5d2472587fc690fb6f86e8b961
Thomas Gordon <nrgetik@users.noreply.github.com>
2022-10-11 10:26:50 -0400

    Handle positional arguments
    Signed-off-by: Thomas Gordon <nrgetik@users.noreply.github.com>

--- minimal_case.py
+++ minimal_case.py
@@ -1,5 +1,6 @@
 from json import dumps
 
-def main(**kwargs):
+def main(*args, **kwargs):
+    kwargs["positional"] = args
     return dumps(kwargs)
     # return { "statusCode": 200, "body": "Status OK" }
