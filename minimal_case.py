f370a0deacf094b185b6fa417acfaff8cbecf093
Thomas Gordon <nrgetik@users.noreply.github.com>
2022-10-11 10:29:52 -0400

    Debug
    Signed-off-by: Thomas Gordon <nrgetik@users.noreply.github.com>

--- minimal_case.py
+++ minimal_case.py
@@ -1,6 +1,11 @@
 from json import dumps
+from pprint import pprint
 
 def main(*args, **kwargs):
-    kwargs["positional"] = args
-    return dumps(kwargs)
+    print(type(*args))
+    print(type(**kwargs))
+    pprint(args)
+    pprint(kwargs)
+    # kwargs["positional"] = args
+    # return dumps(kwargs)
     # return { "statusCode": 200, "body": "Status OK" }
