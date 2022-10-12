9a3399c24015b18062cfbc82833c1bc591fffb13
Thomas Gordon <nrgetik@users.noreply.github.com>
2022-10-12 10:13:36 -0400

    Maybe cover another use case
    Signed-off-by: Thomas Gordon <nrgetik@users.noreply.github.com>

--- starter_ops.py
+++ starter_ops.py
@@ -89,7 +89,7 @@ def test():
     repos = []
     for repo in gh_request("GET", f"/orgs/{GH_ORG_NAME}/repos").json():
         repos.append(repo["full_name"])
-    return repos
+    return dumps(repos)
 
 @app.route("/oven", methods=["POST"])
 def oven():
